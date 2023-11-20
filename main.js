60c37eb9dc43245e62bba962d662e6db7be9a83d
j3van <146213564+j3van@users.noreply.github.com>
2023-11-20 09:46:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById("first").addEventListener("click", f1);
+
+function f1() {
+  alert("sack");
+}
+
+document.getElementById("Second").addEventListener("click", f2);
+
+function f2() {
+  alert("There");
+}
+
+document.getElementById("Thrid").addEventListener("click", f3);
+
+function f3() {
+  alert("Science");
+}
+
+document.getElementById("Fourth").addEventListener("click", f4);
+
+function f4() {
+  alert("The Hook");
+}
+
+document.getElementById("Fifth").addEventListener("click", f5);
+
+function f5() {
+  alert("No Gain");
+}
